fd312576fba2a25aaf0b3058747cae3cc3b55351
Brant Faircloth <brant.faircloth@gmail.com>
2012-08-06 16:48:29 -0700

    single digest, prettified output

--- chopsuey_single_digest.py
+++ chopsuey_single_digest.py
@@ -10,11 +10,17 @@ Copyright (c) 2012 Brant C. Faircloth. All rights reserved.
 Description: Return summary data from a single virual digest of a directory of
 targets using one or more restriction enzymes.
 
+Usage:
+
+    python chopsuey_single_digest.py /folder/with/some/fastas PstI SbfI BfaI NdeI AseI MseI
+
 """
 
 import os
 import pdb
 import glob
+import math
+import numpy
 import argparse
 from Bio import SeqIO
 from Bio import Restriction
@@ -31,7 +37,8 @@ class FullPaths(argparse.Action):
 def get_args():
     """Get arguments from CLI"""
     parser = argparse.ArgumentParser(
-            description="""Program description""")
+            description="""Return summary data from a single virual digest of a directory of
+targets""")
     parser.add_argument(
             "dna",
             action=FullPaths,
@@ -57,32 +64,43 @@ def main():
     args = get_args()
     fastas = get_all_fasta_files(args.dna)
     results = {}
+    print """
+  ___  _  _   __  ____    ____  _  _  ____  _  _
+ / __)/ )( \ /  \(  _ \  / ___)/ )( \(  __)( \/ )
+( (__ ) __ ((  O )) __/  \___ \) \/ ( ) _)  )  /
+ \___)\_)(_/ \__/(__)    (____/\____/(____)(__/
+
+         Virtual Enzyme Single Digest
+"""
     for fasta in fastas:
         fasta_name = os.path.basename(fasta)
-        print "Working on {}...".format(fasta)
+        print "Working on {}...".format(fasta_name)
         fasta_results = {}
         seq = SeqIO.read(open(fasta, 'rU'), 'fasta')
+        print "\tCutting\n\t======================"
         for attr, value in Restriction.__dict__.iteritems():
             if attr in args.enzymes:
-                name = attr
                 cut_site = value.elucidate()
                 cuts = sorted(value.search(seq.seq))
-                lengths = [v - cuts[k - 1] for k, v in enumerate(cuts[1:])]
-                fasta_results[name] = {
+                lengths = [v - cuts[k] for k, v in enumerate(cuts[1:])]
+                print "\tEnzyme {}...".format(attr)
+                fasta_results[attr] = {
                         'cut_site': cut_site,
                         'cuts': len(cuts),
                         'cuts_per_bp': float(len(cuts)) / len(seq.seq),
-                        'cut_length': sum(lengths) / len(lengths)
+                        'cut_length': numpy.mean(lengths),
+                        'cut_95_ci': 1.96 * numpy.std(lengths, ddof=1) / math.sqrt(len(lengths)),
+                        'min': min([i for i in lengths if i > 0]),
+                        'max': max(lengths)
                     }
         results[fasta_name] = fasta_results
-    print "\nFile,Enzyme,Cuts,Cuts per bp,Cut Site,Avg. Cut Length"
+    print "\nFile,Enzyme,Cuts,Cuts per bp,Cut Site,Avg. fragment length,95CI cut length,Min(size),Max(size)"
     for fasta_name, values in results.iteritems():
         for enzyme_name, data in values.iteritems():
-            print "{0},{1},{2[cuts]},{2[cuts_per_bp]},{2[cut_site]},{2[cut_length]}".format(
+            print "{0},{1},{2[cuts]},{2[cuts_per_bp]},{2[cut_site]},{2[cut_length]},{2[cut_95_ci]},{2[min]},{2[max]}".format(
                     fasta_name,
                     enzyme_name,
                     data)
-    pdb.set_trace()
 
 if __name__ == '__main__':
     main()
