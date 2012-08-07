52918ca885265441ce9b6043c7ebec26e6f1ac22
Brant Faircloth <brant.faircloth@gmail.com>
2012-08-07 13:42:42 -0700

    working version with --opposite-ends and --filtering

--- chopsuey_double_digest.py
+++ chopsuey_double_digest.py
@@ -24,6 +24,7 @@ import glob
 import math
 import numpy
 import argparse
+import itertools
 from Bio import SeqIO
 from Bio import Restriction
 
@@ -57,6 +58,20 @@ targets""")
             nargs='+',
             help="""The additional enzyme(s) you will cut with""",
         )
+    parser.add_argument(
+            "--opposite-ends",
+            dest='opposite_ends',
+            action="store_true",
+            default=False,
+            help="""Ensure one end goes with one enzyme, the other end with the other""",
+        )
+    parser.add_argument(
+            "--filter",
+            type=int,
+            default=None,
+            nargs='+',
+            help="""Filter the resulting fragment size distribution""",
+        )
     return parser.parse_args()
 
 
@@ -65,6 +80,86 @@ def get_all_fasta_files(dna):
     for ext in ['*.fasta', '*.fsa', '*.fa', '*.fas']:
         fastas.extend(glob.glob(os.path.join(dna, ext)))
     return fastas
+
+
+def get_single_digest(args, seq):
+    fasta_results = {}
+    print "\tCutting\n\t======================"
+    for attr, value in Restriction.__dict__.iteritems():
+        if (attr in args.first) or (attr in args.others):
+            cut_site = value.elucidate()
+            cuts = sorted(value.search(seq.seq))
+            print "\tEnzyme {}...".format(attr)
+            fasta_results[attr] = {
+                    'cut_site': cut_site,
+                    'cuts': cuts,
+                    'seq_len': len(seq.seq)
+                }
+    return fasta_results
+
+
+def get_double_digest(args, single_cuts, seq):
+    cut_results = {}
+    base = single_cuts[args.first]
+    print "\tDouble-cutting\n\t======================"
+    for enzyme, values in single_cuts.iteritems():
+        base_cuts = base['cuts']
+        base_sites = base['cut_site'].replace('_', '').split('^')
+        all_cuts = base_cuts
+        if enzyme != args.first:
+            compare_cuts = values['cuts']
+            compare_sites = values['cut_site'].replace('_', '').split('^')
+            all_cuts.extend(compare_cuts)
+            all_cuts = sorted(list(set(all_cuts)))
+
+            if args.opposite_ends:
+                #pdb.set_trace()
+                ends = ([base_sites[1], compare_sites[0]], [compare_sites[1], base_sites[0]])
+                fragments = []
+                for k, stop in enumerate(all_cuts[1:]):
+                    start = all_cuts[k]
+                    slc = seq[start - 1:stop - 1].seq.upper()
+                    for end in ends:
+                        if slc.startswith(end[0]) and slc.endswith(end[1]):
+                            if args.filter is not None:
+                                if args.filter[0] <= len(slc) <= args.filter[1]:
+                                    fragments.append(len(slc))
+                            else:
+                                fragments.append(len(slc))
+                            break
+                #pdb.set_trace()
+                combo = "{0}-{1}".format(args.first, enzyme)
+                print "\tCombo {}...".format(combo)
+                cut_results[combo] = {
+                        'cuts': len(fragments),
+                        'cuts_per_bp': float(len(fragments)) / values['seq_len'],
+                        'cut_length': numpy.mean(fragments),
+                        'cut_95_ci': 1.96 * numpy.std(fragments, ddof=1) / math.sqrt(len(fragments)),
+                        'min': min([i for i in fragments if i > 0]),
+                        'max': max(fragments)
+                    }
+
+            else:
+                lengths = []
+                for k, v in enumerate(all_cuts[1:]):
+                    slc = v - all_cuts[k]
+                    if args.filter is not None:
+                        if args.filter[0] <= slc <= args.filter[1]:
+                            lengths.append(slc)
+                    else:
+                        lengths.append(slc)
+                combo = "{0}-{1}".format(args.first, enzyme)
+                print "\tCombo {}...".format(combo)
+                cut_results[combo] = {
+                        'cuts': len(all_cuts),
+                        'cuts_per_bp': float(len(all_cuts)) / values['seq_len'],
+                        'cut_length': numpy.mean(lengths),
+                        'cut_95_ci': 1.96 * numpy.std(lengths, ddof=1) / math.sqrt(len(lengths)),
+                        'min': min([i for i in lengths if i > 0]),
+                        'max': max(lengths)
+                    }
+    #pdb.set_trace()
+    return cut_results
 
 
 def main():
@@ -82,40 +177,9 @@ def main():
     for fasta in fastas:
         fasta_name = os.path.basename(fasta)
         print "\nWorking on {}...".format(fasta_name)
-        fasta_results = {}
         seq = SeqIO.read(open(fasta, 'rU'), 'fasta')
-        print "\tCutting\n\t======================"
-        for attr, value in Restriction.__dict__.iteritems():
-            if (attr in args.first) or (attr in args.others):
-                cut_site = value.elucidate()
-                cuts = sorted(value.search(seq.seq))
-                print "\tEnzyme {}...".format(attr)
-                fasta_results[attr] = {
-                        'cut_site': cut_site,
-                        'cuts': cuts,
-                        'seq_len': len(seq.seq)
-                    }
-        cut_results = {}
-        base = fasta_results[args.first]
-        print "\tDouble-cutting\n\t======================"
-        for enzyme, values in fasta_results.iteritems():
-            all_cuts = base['cuts']
-            if enzyme != args.first:
-                all_cuts.extend(values['cuts'])
-                all_cuts = sorted(all_cuts)
-                lengths = [v - all_cuts[k] for k, v in enumerate(all_cuts[1:])]
-                #pdb.set_trace()
-                combo = "{0}-{1}".format(args.first, enzyme)
-                print "\tCombo {}...".format(combo)
-                cut_results[combo] = {
-                        'cuts': len(all_cuts),
-                        'cuts_per_bp': float(len(all_cuts)) / values['seq_len'],
-                        'cut_length': numpy.mean(lengths),
-                        'cut_95_ci': 1.96 * numpy.std(lengths, ddof=1) / math.sqrt(len(lengths)),
-                        'min': min([i for i in lengths if i > 0]),
-                        'max': max(lengths)
-                    }
-        results[fasta_name] = cut_results
+        single_cuts = get_single_digest(args, seq)
+        results[fasta_name] = get_double_digest(args, single_cuts, seq)
         #pdb.set_trace()
     print "\nFile,Enzyme Combo,Cuts,Cuts per bp,Avg. fragment length,95CI cut length,Min(size),Max(size)"
     for fasta_name, values in results.iteritems():
